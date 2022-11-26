231ae453f7a569968aacc0691c69057b9e963f38
svlgerasimov <svlgerasimov@yandex.ru>
2022-11-26 12:43:46 +0300

    style: убрать лишние todo аннотации

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -9,9 +9,6 @@ import ru.practicum.shareit.user.service.UserService;
 
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
