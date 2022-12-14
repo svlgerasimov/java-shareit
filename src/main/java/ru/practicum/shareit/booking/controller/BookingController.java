b43378e5eb4a993e5902724718a45128807e9274
svlgerasimov <svlgerasimov@yandex.ru>
2022-12-14 13:05:42 +0300

    style: убрать TODO ветки add-bookings

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -12,9 +12,6 @@ import ru.practicum.shareit.exception.CustomValidationException;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
