6a82f2549f3eac2d9063655f39cb994305de6e7a
Sergey Gerasimov <svlgerasimov@yandex.ru>
2022-12-28 13:42:36 +0300

    feat: сигнатура методов ItemRequestController

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -3,8 +3,10 @@ package ru.practicum.shareit.request.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDtoIn;
 import ru.practicum.shareit.request.dto.ItemRequestDtoOut;
+import ru.practicum.shareit.request.dto.ItemRequestDtoOutExtended;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/requests")
@@ -15,6 +17,25 @@ public class ItemRequestController {
                                  @RequestHeader("X-Sharer-User-Id") long userId) {
         return null;
     }
+
+    @GetMapping
+    public List<ItemRequestDtoOutExtended> findByUser(@RequestHeader("X-Sharer-User-Id") long userId) {
+        return null;
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDtoOutExtended> findAllByOtherUsers(@RequestParam long from, @RequestParam int size,
+                                                   @RequestHeader("X-Sharer-User-Id") long userId) {
+        return null;
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDtoOutExtended findById(long requestId) {
+        return null;
+    }
+
+
+
 
 
 }
