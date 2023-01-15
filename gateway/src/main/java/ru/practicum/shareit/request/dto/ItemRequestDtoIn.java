0f4f439cdffa327ee73163d814f9de2f398ef1bf
svlgerasimov <svlgerasimov@yandex.ru>
2023-01-15 21:53:34 +0300

    refactor: перенести валидацию ItemRequest в gateway

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoIn.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoIn.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
+import javax.validation.constraints.NotBlank;
+
 @Value
 public class ItemRequestDtoIn {
 
+    @NotBlank
     String description;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -2,28 +2,23 @@ package ru.practicum.shareit.request.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDtoIn;
 import ru.practicum.shareit.request.dto.ItemRequestDtoOut;
 import ru.practicum.shareit.request.dto.ItemRequestDtoOutExtended;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/requests")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-@Validated
 public class ItemRequestController {
 
     private final ItemRequestService itemRequestService;
 
     @PostMapping
-    public ItemRequestDtoOut add(@RequestBody @Valid ItemRequestDtoIn itemRequestDto,
+    public ItemRequestDtoOut add(@RequestBody ItemRequestDtoIn itemRequestDto,
                                  @RequestHeader("X-Sharer-User-Id") long userId) {
         return itemRequestService.add(itemRequestDto, userId);
     }
@@ -35,8 +30,8 @@ public class ItemRequestController {
 
     @GetMapping("/all")
     public List<ItemRequestDtoOutExtended> findAllByOtherUsers(
-            @RequestParam(defaultValue = "0") @PositiveOrZero Long from,
-            @RequestParam(defaultValue = "10") @Positive Integer size,
+            @RequestParam Long from,
+            @RequestParam Integer size,
             @RequestHeader("X-Sharer-User-Id") long userId) {
         return itemRequestService.findByOtherUsers(userId, from, size);
     }
