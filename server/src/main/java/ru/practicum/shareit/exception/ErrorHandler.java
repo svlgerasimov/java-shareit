ab6bf19042670e6c850f150531a33199e65bfccc
Sergey Gerasimov <svlgerasimov@yandex.ru>
2023-01-16 16:07:09 +0300

    refactor: оставить в общем pom.xml общие зависимости, специфичные перенести в дочерние pom модулей

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -4,17 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -65,32 +61,6 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Map<String, String> handleAuthenticationErrorException(AuthenticationErrorException exception) {
         Map<String, String> result = Map.of("Authentication Fail", exception.getMessage());
-        log.warn(String.valueOf(result), exception);
-        return result;
-    }
-
-    // Ошибка валидации полей десериализируемого объекта @Valid
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        Map<String, String> result = exception.getAllErrors().stream()
-                .collect(Collectors.toMap(
-                        objectError ->
-                                objectError instanceof FieldError ?
-                                        "Validation error in field " + ((FieldError) objectError).getField() +
-                                                " with value " + ((FieldError) objectError).getRejectedValue() :
-                                        "Validation Error in " + objectError.getObjectName(),
-                        objectError -> Objects.requireNonNullElse(objectError.getDefaultMessage(), "")
-                ));
-        log.warn(String.valueOf(result), exception);
-        return result;
-    }
-
-    // Ошибка валидации параметра запроса @Validated
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleConstraintViolationException(ConstraintViolationException exception) {
-        Map<String, String> result = Map.of("Bad Request", exception.getMessage());
         log.warn(String.valueOf(result), exception);
         return result;
     }
